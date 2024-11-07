3cdb049ca256e22615a0a23e1eb908383576571b
seani73 <sean.irwin.007@gmail.com>
2024-11-07 16:42:29 +0000

    Start of pairwise_offset.py

--- challenge/pairwise_offset.py
+++ challenge/pairwise_offset.py
@@ -1,2 +1,22 @@
-def pairwise_offset(sequence, fillvalue, offset):
-    pass
+def pairwise_offset(sequence, fillvalue='*', offset=0):
+    outputList = []
+    seqLength = len(sequence)
+    i = 0
+
+    if offset == 0:
+        for item in sequence:
+            outputList.append((item, item))
+    else:
+        while item in sequence:
+            if i <= offset:
+                outputList.append((item, fillvalue))
+                i += 1
+            else:
+                outputList.append()
+                #Need to know the length of the sequence and the offset to know how many there well be
+                #to know if there will be (item, item) tuples
+
+
+    return None
+
+print(pairwise_offset(['a', 'b', 'c']))
