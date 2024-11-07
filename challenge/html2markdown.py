bfc31fb4625838edd1d5243ff3b59558d9cb265d
seani73 <sean.irwin.007@gmail.com>
2024-11-07 12:43:40 +0000

    First attempt html2markdown

--- challenge/html2markdown.py
+++ challenge/html2markdown.py
@@ -1,4 +1,19 @@
+import re
+
+patternItalics = re.compile(r'<\/?em>')
+patternSpaces = re.compile(r'( \n*)')
+patternParas = re.compile(r'<\/?p>')
+patternURLs = re.compile(r'<a href="(.+?)">(.+?)</a>')
+
+#<a href="https://www.example.com">Visit Example</a>
+#[Visit Example](https://www.example.com)
 
 def html2markdown(html):
     '''Take in html text as input and return markdown'''
-    pass
+
+    markdown = re.sub(patternItalics, '*', html)
+    markdown = re.sub(patternSpaces, ' ', markdown)
+    markdown = re.sub(patternParas, '\n\n', markdown)
+    markdown = re.sub(patternURLs, r'[\2](\1)', markdown)
+
+    return markdown.strip()
