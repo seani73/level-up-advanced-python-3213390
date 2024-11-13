c5268c5270a843cd79dec5ab0a20bd7fbaa2a9b9
seani73 <sean.irwin.007@gmail.com>
2024-11-13 15:37:04 +0000

    Start of age_slowest_race.py

--- challenge/age_slowest_race.py
+++ challenge/age_slowest_race.py
@@ -2,8 +2,11 @@
 # This dataset has race times for women 10k runners from the Association of Road Racing Statisticians
 # Assume a year has 365.25 days
 
+import re
+import datetime
+
 def get_data():
-    with open('10k_racetimes.txt', 'rt') as file:
+    with open('challenge/10k_racetimes.txt', 'rt') as file:
         content = file.read()
     return content
 
@@ -11,9 +14,17 @@ def get_event_time(line):
     """Given a line with Jennifer Rhines' race times from 10k_racetimes.txt, 
        parse it and return a tuple of (age at event, race time).
        Assume a year has 365.25 days"""
-    pass
-    
+    dates = re.findall(r'\d{2} \w{3} \d{4}', line)
+    raceDate = datetime.datetime.strptime(dates[0], '%d %b %Y')
+    doB = datetime.datetime.strptime(dates[1], '%d %b %Y')
+
+    age = raceDate.year() - doB.year()
+
+    if raceDate.month() < doB.month():
+        age -= 1
+
 def get_age_slowest_times():
     '''Return a tuple (age, race_time) where:
        age: AyBd is in this format where A and B are integers'''
     races = get_data()
+
