1e5258545fc3384be6ccd9259267e361bf38f14f
seani73 <sean.irwin.007@gmail.com>
2024-11-18 14:40:35 +0000

    calculator setup

--- challenge/calculator.py
+++ challenge/calculator.py
@@ -1,2 +1,9 @@
 class Calculator:
-    pass
+    def __init__(self, null): #Need to replace null with something here
+        pass
+
+    def __enter(self):
+        pass
+
+    def __exit__(self, exc_type, exc_value, exc,traceback):
+        pass
